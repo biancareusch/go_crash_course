5751f02232032901d3795d5b28cb8d73282d32ee
biancareusch <reusch.bianca@gmail.com>
2021-02-20 10:29:04 -0600

    FizzBuzz

--- 07_loops/main.go
+++ 07_loops/main.go
@@ -12,8 +12,20 @@ func main() {
 	}
 
 	//short method
-	for i := 1; 1 <= 10; i++ {
+	for i := 1; i <= 10; i++ {
 		fmt.Printf("Number %d\n", i)
 	}
 
+	//Fizz buzz
+	for i := 1; i <= 100; i++ {
+		if i%15 == 0 {
+			fmt.Println("FizzBuzz")
+		} else if i%3 == 0 {
+			fmt.Println("Fizz")
+		} else if i%5 == 0 {
+			fmt.Println("Buzz")
+		} else {
+			fmt.Println(i)
+		}
+	}
 }
